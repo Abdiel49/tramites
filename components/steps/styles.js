24dca2574fb7df4708539690770a7365819993e1
abdiel249 <abdielorellan3@gmail.com>
2021-10-28 16:33:08 -0400

    create steps styles and implement in Step One

--- components/steps/styles.js
+++ components/steps/styles.js
@@ -1,8 +1,62 @@
-// screens
-// └── procedure
-// |    └── Inscripcion.js
-// |    └── TramiteBasico.js
-// └── styles
-//     └── procedure
-//           └── Inscripcion.js
-//           └── TramiteBasico.js
+import { StyleSheet } from "react-native";
+import { colors } from "../../styles/colors";
+
+export const styles = StyleSheet.create({
+  stepContainer: {
+    backgroundColor: colors.grey,
+    borderRadius: 15,
+    margin: 10,
+    padding: 10,
+  },
+  stepNavbar: {
+    display: "flex",
+    flexDirection: "row",
+    flexGrow: 1,
+    alignContent: "center",
+  },
+  stepNavbar_info: {
+    display: "flex",
+    flex: 1,
+    margin: 5,
+    padding: 3,
+  },
+  stepNumber: {
+    fontWeight: "bold",
+  },
+  stepTitle: {
+    // flex:1,
+    // flexWrap:'wrap',
+    // flexShrink: 1,
+  },
+  stepName: {},
+  stepDataContainer: {},
+  textBolt: {
+    fontWeight: "bold",
+  },
+  containerSimple: {
+    marginTop: 5,
+  },
+  containerList: {
+    display: "flex",
+    flex: 1,
+    marginLeft: 5,
+  },
+  testListItem: {
+    marginVertical: 5,
+  },
+  textItalic: {
+    fontStyle: "italic",
+  },
+  rowTitle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowTitleText: {
+    alignContent: "center",
+    display: "flex",
+    flex: 1,
+    flexGrow: 1,
+    margin: 5,
+    padding: 3,
+  },
+})
