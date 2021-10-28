a0ac7b9024a7238b82b34175d285c5f0294c7bc3
abdiel249 <abdielorellan3@gmail.com>
2021-10-28 16:25:25 -0400

    delete navbar style implemente safeAreaContainer into gStyles (global styles)

--- screens/procedure/Procedure.js
+++ screens/procedure/Procedure.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { View, SafeAreaView, ScrollView } from "react-native";
 import StepFooter from "../../components/footer/StepFooter";
 import Navbar from "../../components/nav/Navbar";
 import Step from "../../components/steps/Step";
 import Step2 from "../../components/steps/Step2";
 import Step3 from "../../components/steps/Step3";
 import Step4 from "../../components/steps/Step4";
-import { colors } from "../../styles/colors";
+import { gStyles } from "../../styles/gStyles";
+
 
 export default function Procedure() {
   return (
     <View>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={ gStyles.safeAreaContainer }>
         <Navbar title="Inscripcion a una carrera" />
-        <ScrollView style={styles.scrollView}>
+        <ScrollView>
           <Step stepNumber="Paso 1" />
           <Step2 stepNumber="Paso 2" />
           <Step3 stepNumber="Paso 3" />
@@ -24,9 +25,3 @@ export default function Procedure() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.blue,
-  },
-});
