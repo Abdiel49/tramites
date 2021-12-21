01f0b290a58e4f974368494f87a651cfa1517671
adrianC56 <adriancrespog3@gmail.com>
2021-12-21 19:55:28 -0400

    Buscador implementado

--- app/components/home/HomeScreen.js
+++ app/components/home/HomeScreen.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import {
+  View,
   StyleSheet,
   Text,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  TextInput
 } from "react-native";
 import axios from "axios";
 import { networkEnv } from "../../../network";
+import LupaIcon from "./LupaIcon";
 
 const Home = ({ navigation }) => {
-  const [tramites, setTramites] = useState([]);
+  const [tramites, setTramites] = useState(null);
   const [apiBase, setApiBase] = useState(networkEnv);
+  const [textoBuscador, setTextoBuscador] = useState('');
+  const [tramitesFilt, setTramitesFilt] = useState(null);
 
   useEffect(() => {
     let isApiSubscribed = true;
@@ -19,6 +24,7 @@ const Home = ({ navigation }) => {
       .then((res) => {
         if (isApiSubscribed) {
           setTramites(res.data);
+          setTramitesFilt(res.data);
         }
       })
       .catch((err) => {
@@ -29,31 +35,122 @@ const Home = ({ navigation }) => {
     };
   }, [apiBase]);
 
-  return (
-    <ScrollView>
-      <TouchableOpacity
-        testID="btm-tramite"
-        style={styles.buttonStyle}
-        onPress={() => navigation.navigate("tramiteInscripcion")}
-      >
-        <Text style={styles.texteStyle}>Inscripción a una Carrera</Text>
-      </TouchableOpacity>
-      {
-        tramites.map((tramite) => (
-          <TouchableOpacity
-            testID="btm-tramite"
-            key={tramite.id}
-            style={styles.buttonStyle}
-            onPress={() =>
-              navigation.navigate("tramite", {
-                tramite,
-              })
+  const quitarTildes = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  } 
+
+  const buscar = () => {
+    if (textoBuscador != '') {
+      const textoLimpio = quitarTildes(textoBuscador.toLowerCase());
+
+      const resulBusq = tramites.filter((tramite) => {
+        let cumple = false
+        const nombreTramite = quitarTildes(tramite.titulo.toLowerCase());
+        const descTramite = quitarTildes(tramite.descripcion.toLowerCase());
+        let nombUbicacion = null;
+        let nombUbicacionReq = [];
+  
+        if (tramite.mapData) {
+          nombUbicacion = quitarTildes(tramite.mapData.locationTitle.toLowerCase());
+        }
+  
+        tramite.requisitos.forEach(requisito => {
+          if (requisito.mapData) {
+            nombUbicacionReq.push(quitarTildes(requisito.mapData.locationTitle.toLowerCase()));
+          }
+        });
+  
+        if (nombreTramite.includes(textoLimpio)
+            || descTramite.includes(textoLimpio)) {
+              cumple = true;
+        } else if (nombUbicacion){
+          if (nombUbicacion.includes(textoLimpio)) {
+            cumple = true;
+          }
+        }
+        
+        if (nombUbicacionReq.length > 0){
+          nombUbicacionReq.forEach(ubicEnReq => {
+            if (ubicEnReq.includes(textoLimpio)) {
+              cumple = true;
             }
-          >
-            <Text style={styles.texteStyle}>{tramite.titulo}</Text>
-          </TouchableOpacity>
-        ))}
-    </ScrollView>
+          });
+        }
+  
+        return cumple;
+      })
+      setTramitesFilt(resulBusq);
+    }
+  };
+
+  useEffect(() => {
+    if (textoBuscador == '') {
+      setTramitesFilt(tramites);
+    } else {
+      buscar();
+    }
+  }, [textoBuscador])
+
+  return (
+    <View>
+      <View style={styles.contBuscador}>
+        <TextInput
+          style={styles.buscador}
+          onChangeText={setTextoBuscador}
+          value={textoBuscador}
+          placeholder="Buscar"
+        />
+        <TouchableOpacity
+          style={styles.sCbuttonStyle}
+          onPress={() => buscar()}
+        >
+          <LupaIcon/>                 
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        <TouchableOpacity
+          testID="btm-tramite"
+          style={styles.buttonStyle}
+          onPress={() => navigation.navigate("tramiteInscripcion")}
+        >
+          <Text style={styles.texteStyle}>Inscripción a una Carrera</Text>
+        </TouchableOpacity>
+        {
+          (tramitesFilt)?
+          ( 
+            tramitesFilt.length > 0?(
+              tramitesFilt.map((tramite) => (
+                <TouchableOpacity
+                  testID="btm-tramite"
+                  key={tramite.id}
+                  style={styles.buttonStyle}
+                  onPress={() =>
+                    navigation.navigate("tramite", {
+                      tramite,
+                    })
+                  }
+                >
+                  <Text style={styles.texteStyle}>{tramite.titulo}</Text>
+                </TouchableOpacity>
+              ))
+            )
+            :
+            <Text style={styles.textoInfo}>
+              No se encontró algún tramite con la información que buscas
+            </Text>
+          )  
+          :
+          (
+          <Text style={styles.textoInfo}>
+            Cargando...
+          </Text>
+          )
+        }
+      <View style={styles.espaciado}>
+
+      </View>
+      </ScrollView>
+    </View>
   );
 };
 
@@ -67,10 +164,54 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 
+  sCbuttonStyle: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#0F354A",
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: 'column',
+    marginTop: 5
+  },
+
   texteStyle: {
     color: "white",
     fontSize: 17,
   },
+
+  buscador: {
+    marginVertical: 10,
+    marginRight: 10,
+    width: '75%',
+    height: 45,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    padding: 10,
+    fontSize: 16,
+    flexDirection: 'column'
+  },
+
+  espaciado: {
+    padding: 50
+  },
+
+  contBuscador: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+
+  lupaIcon: {
+    width: '100%',
+    height: '100%'
+  },
+
+  textoInfo: {
+    fontSize: 30,
+    marginVertical: '30%',
+    marginHorizontal: 20,
+    textAlign: 'center'
+  }
 });
 
 export default Home;
